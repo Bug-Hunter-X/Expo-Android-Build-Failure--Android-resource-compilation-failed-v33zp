ec5caaa9a000f68f85b4c8aefe471ca794b88234
BugHunter-X <askzenai@gmail.com>
2025-01-19 06:46:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+This error occurs when using the Expo CLI to build an Android app.  The build process fails with the following error message:
+
+`Execution failed for task ':app:processDebugResources'.
+> A failure occurred while executing com.android.build.gradle.internal.res.ResourceCompilerRunnable
+   > Android resource compilation failed`
+
+This error is often caused by issues with your app's resources, such as invalid XML files, incorrect image dimensions, or missing assets.
